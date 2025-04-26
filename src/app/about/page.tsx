3a296449c3421e7c95a83db99a7f0654b5a611bd
nellaiesevai <nellaiesevai@gmail.com>
2025-04-26 11:29:37 +0000

    firebase studio update

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+tsx
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/app/contact/page.tsx
@@ -0,0 +1,8 @@
+tsx
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Us</h1>
+    </div>
+  );
+}
